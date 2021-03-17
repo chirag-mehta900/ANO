338aa2ffd51adcc51f8aaed463ab0374a71c73d2
kenil7142 <bkenil.spt@gmail.com>
2021-03-17 11:32:59 +0530

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, HomeComponent, FooterComponent, MapComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
