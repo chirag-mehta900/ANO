22d74ea253ea6706af09967468fb09f74825a693
kenil7142 <bkenil.spt@gmail.com>
2021-03-17 18:24:43 +0530

    map page

--- src/@theme/shared.module.ts
+++ src/@theme/shared.module.ts
@@ -1,15 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-const NB_Module = [
-  AgmCoreModule.forRoot({
-    apiKey: '',
-  }),
-];
+const NB_Module = [NgbModule];
 @NgModule({
   imports: [CommonModule, ...NB_Module],
-  exports: [CommonModule],
+  exports: [CommonModule, ...NB_Module],
   providers: [],
 })
 export class SharedModule {}
