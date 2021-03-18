6d3ec09079cdbb26a1d9b94cb6d8b4cef1a43ae2
chirag-mehta900 <chirag.smartypantstech@gmail.com>
2021-03-18 11:27:10 +0530

    o

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,37 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-lat:any 
-log:any
+
   constructor() { }
 
   ngOnInit() {
-    if (!navigator.geolocation) {
-      console.log('location not found');
-    }
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.lat = position.coords.latitude;
-      this.log = position.coords.longitude;
-      console.log(this.lat,this.log);
-    });
+
   }
-  watchPosition() {
-    navigator.geolocation.watchPosition(
-      (position) => {
-        console.log(
-          `lat: ${position.coords.latitude}, log: ${position.coords.longitude}`
-        );
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0,
-      }
-    );
-  }
-  }
+}
 
 
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,10 +10,37 @@ export class MapComponent implements OnInit {
   title = 'My first AGM project';
   lat = 21.1588329;
   lng = 72.7688111;
+
   constructor(config: NgbRatingConfig) {
     config.max = 5;
     config.readonly = true;
   }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    if (!navigator.geolocation) {
+      console.log('location not found');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.lat = position.coords.latitude;
+      this.lng = position.coords.longitude;
+      console.log(this.lat,this.lng);
+    });
+  }
+  // watchPosition() {
+  //   navigator.geolocation.watchPosition(
+  //     (position) => {
+  //       console.log(
+  //         `lat: ${position.coords.latitude}, log: ${position.coords.longitude}`
+  //       );
+  //     },
+  //     (err) => {
+  //       console.log(err);
+  //     },
+  //     {
+  //       enableHighAccuracy: true,
+  //       timeout: 5000,
+  //       maximumAge: 0,
+  //     }
+  //   );
+  // }
 }
