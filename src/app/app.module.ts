7df26d57145f26af80e5750ffd25cedc3803d0f7
chirag-mehta900 <chirag.smartypantstech@gmail.com>
2021-03-30 11:13:52 +0530

    contact page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { TrackingmapComponent } from './trackingmap/trackingmap.component';
 import { CartComponent } from './cart/cart.component';
 import { AgmDirectionModule } from 'agm-direction';
 import { AddProductComponent } from './cart/add-product/add-product.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { AddProductComponent } from './cart/add-product/add-product.component';
     TrackingmapComponent,
     CartComponent,
     AddProductComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
