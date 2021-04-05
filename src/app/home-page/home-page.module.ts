f0cc6ef1c0b8ee3cc782f5ccab7f92699d8845a3
neha433 <neha.smartypantstech@gmail.com>
2021-04-05 15:03:51 +0530

    time conversition complete

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -5,9 +5,10 @@ import { HomePageRoutingModule } from "./home-page-routing.module";
 import { HomePageComponent } from "./home-page.component";
 import { HomeDetailsComponent } from "./home-details/home-details.component";
 import { SharedModule } from "src/@theme/shared.module";
+import { DriverComponent } from './driver/driver.component';
 
 @NgModule({
-  declarations: [HomePageComponent, HomeDetailsComponent],
+  declarations: [HomePageComponent, HomeDetailsComponent, DriverComponent],
   imports: [CommonModule, HomePageRoutingModule, SharedModule],
 })
 export class HomePageModule {}
