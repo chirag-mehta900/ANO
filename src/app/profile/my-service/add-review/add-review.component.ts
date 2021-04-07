d1e5cd994505ac3c2ee103c0100187477ce11107
kenil7142 <bkenil.spt@gmail.com>
2021-04-07 19:23:07 +0530

    rating

--- src/app/profile/my-service/add-review/add-review.component.ts
+++ src/app/profile/my-service/add-review/add-review.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ProfileService } from 'src/@theme/Services/profile.service';
 
 @Component({
   selector: 'app-add-review',
@@ -7,13 +9,58 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./add-review.component.css'],
 })
 export class AddReviewComponent implements OnInit {
-  rating3 = 3;
+  rating = 0;
+  public form: FormGroup;
+  Rating: boolean = false;
+  Title: boolean = false;
+  Comment: boolean = false;
 
-  constructor(private activeModal: NgbActiveModal) {}
+  constructor(
+    private activeModal: NgbActiveModal,
+    private profile: ProfileService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.Rating = false;
+    this.Title = false;
+    this.Comment = false;
+
+    this.form = new FormGroup({
+      rating: new FormControl(null, Validators.required),
+      title: new FormControl(null, Validators.required),
+      commet: new FormControl(null, Validators.required),
+    });
+  }
 
   close() {
     this.activeModal.close();
+  }
+
+  submit() {
+    if (
+      !this.form.value.rating ||
+      !this.form.value.title ||
+      !this.form.value.commet
+    ) {
+      if (!this.form.value.rating) {
+        this.Rating = true;
+      } else {
+        this.Rating = false;
+      }
+      if (!this.form.value.title) {
+        this.Title = true;
+      } else {
+        this.Title = false;
+      }
+      if (!this.form.value.commet) {
+        this.Comment = true;
+      } else {
+        this.Comment = false;
+      }
+    } else {
+      this.profile.SubmitReview(this.form.value, 1).subscribe((data) => {
+        console.log(data);
+      });
+    }
   }
 }
