917751ec49617a66b33a4368b11d9c3bc4c1a6a4
kenil7142 <bkenil.spt@gmail.com>
2021-04-07 16:31:25 +0530

    forgot and reset done

--- src/app/profile/change-psw/change-psw.component.ts
+++ src/app/profile/change-psw/change-psw.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ProfileService } from 'src/@theme/Services/profile.service';
 
 @Component({
   selector: 'app-change-psw',
@@ -7,11 +9,47 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./change-psw.component.css'],
 })
 export class ChangePswComponent implements OnInit {
-  constructor(private activeModal: NgbActiveModal) {}
+  changepsw: FormGroup;
+  notmatch: boolean = false;
+  msg: any;
+  constructor(
+    private activeModal: NgbActiveModal,
+    private profile: ProfileService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.changepsw = new FormGroup({
+      old_password: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      confpassword: new FormControl(null, Validators.required),
+    });
+  }
 
   close() {
     this.activeModal.close();
+  }
+  Changepsw() {
+    if (this.changepsw.value.password == this.changepsw.value.confpassword) {
+      this.notmatch = false;
+      console.log(this.changepsw.value);
+      const obj = {
+        old_password: this.changepsw.value.old_password,
+        password: this.changepsw.value.password,
+      };
+
+      this.profile.changepassword(obj).subscribe((data) => {
+        console.log(data);
+        if (data['status'] == 200) {
+          // this.activeModal.close();
+          this.changepsw.reset();
+          this.msg = data['message'];
+        }
+      }),
+        (error) => {
+          console.log(error);
+        };
+    } else {
+      this.notmatch = true;
+    }
   }
 }
