d3f3bfb1db7ad550241faca785f901bacec9b7f6
kenil7142 <bkenil.spt@gmail.com>
2021-04-14 13:14:51 +0530

    edit address done

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddAddressComponent } from './add-address/add-address.component';
 import { AddressComponent } from './address/address.component';
 import { CommentComponent } from './comment/comment.component';
+import { EditAddressComponent } from './edit-address/edit-address.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { MyServiceComponent } from './my-service/my-service.component';
 import { ProfileComponent } from './profile.component';
@@ -11,10 +12,10 @@ const routes: Routes = [
   { path: '', component: ProfileComponent },
   { path: 'address', component: AddressComponent },
   { path: 'edit-user', component: EditUserComponent },
+  { path: 'edit', component: EditAddressComponent },
   { path: 'add-address', component: AddAddressComponent },
   { path: 'comment', component: CommentComponent },
   { path: 'service', component: MyServiceComponent },
-  { path: 'profile/service', component: MyServiceComponent },
 ];
 
 @NgModule({
