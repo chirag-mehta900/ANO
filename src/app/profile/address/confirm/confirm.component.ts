0536d66602904fb7798666681fb7bfaad890cab5
kenil7142 <bkenil.spt@gmail.com>
2021-04-19 16:17:49 +0530

    popup fix and return slot

--- src/app/profile/address/confirm/confirm.component.ts
+++ src/app/profile/address/confirm/confirm.component.ts
@@ -5,33 +5,29 @@ import { ProfileService } from 'src/@theme/Services/profile.service';
 @Component({
   selector: 'app-confirm',
   templateUrl: './confirm.component.html',
-  styleUrls: ['./confirm.component.css']
+  styleUrls: ['./confirm.component.css'],
 })
 export class ConfirmComponent implements OnInit {
-
-  @Output('addressId') addressId;
+  @Output('address') address;
   disableButton: boolean = false;
-  address : any
+  newaddress: any[] = [];
 
   constructor(
     private profile: ProfileService,
     private activeModal: NgbActiveModal
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.profile.getAddressbyID(this.addressId).subscribe((data) => {
-      this.address = data['data'][0].addressLine;
-
-    });
+    this.newaddress.push(this.address);
   }
 
   onSave() {
     this.disableButton = true;
     var data: any;
-    this.profile.makedefault(this.addressId, data).subscribe((data) => {
+    this.profile.makedefault(this.address.id, data).subscribe((data) => {
       console.log(data);
 
-      if(data['status'] == 200){
+      if (data['status'] == 200) {
         this.activeModal.close();
       }
     });
