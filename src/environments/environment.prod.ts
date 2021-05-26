642298bc5b5a28fe0c4d3f603e245c18a069f8dc
chirag-mehta900 <chirag.smartypantstech@gmail.com>
2021-05-26 20:44:06 +0530

    latest build upload heroku API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl:
-    'http://anoapis-env.eba-2vcpuqmp.ap-south-1.elasticbeanstalk.com/api/',
+    'https://ano-apis-v8.herokuapp.com/api/',
 };
